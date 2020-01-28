7ef0d0f85a5d68037719e4c52d9b34a91d3093b9
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-01-28 09:10:14 -0700

    Add flickr service

---
+++ app/services/flickr_service.rb
@@ -0,0 +1,27 @@
+class FlickrService
+  def image_of(word)
+    get_json("/services/rest/?text=#{word}")
+    # get_json("/services/rest/?text=#{word}, skyline")
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: "https://www.flickr.com") do |f|
+      f.params['method'] = "flickr.photos.search"
+      f.params['api_key'] = ENV['FLICKR_API_KEY']
+      f.params['geo_context'] = 2
+      f.params['per_page'] = 1
+      f.params['accuracy'] = 12
+      f.params['safe_search'] = 1
+      f.params['format'] = 'json'
+      f.params['nojsoncallback'] = 1
+      f.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
