fb26d32b8f3cf196ec55aaad6b0af76710ba9257
Nathan Thomas <nathan.gordon.thomas@gmail.com>
2020-02-03 21:00:22 -0700

    Randomize cards

--- app/controllers/study_controller.rb
+++ app/controllers/study_controller.rb
@@ -5,28 +5,30 @@ class StudyController < ApplicationController
     @cards = @deck.cards
     correct_cards = []
     @correct_count = 0
-    incorrect_cards = []
+    @incorrect_cards = []
     @incorrect_count = 0
 
-    @cards.map do |card|
-      if card.correct == true
-        correct_cards << card
-        @correct_count += 1
-      else
-        incorrect_cards << card
+    @cards.each do |card|
+      if card.correct == nil
+        @incorrect_cards << card
+      elsif card.correct == false
+        @incorrect_cards << card
         @incorrect_count += 1
+      else
+        correct_cards << card
+        @correct_count +=1
       end
     end
 
     if @correct_count == @cards.length
         @cards.each do |card|
-          card.correct = false
+          card.correct = nil
           card.save
         end
       redirect_to decks_path
     else
-      @current_card = Card.where(correct: false).first
+      @current_card = @incorrect_cards.shuffle.first
     end
   end
-  
+
 end
